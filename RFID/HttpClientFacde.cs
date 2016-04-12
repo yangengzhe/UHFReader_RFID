ec1c47950f0b5e70c10d54a84b9d31c3f7d90b10
yangengzhe <emailvip@vip.qq.com>
2016-04-12 18:45:00 +0800

    '添加网络模块'

---
+++ RFID/HttpClientFacde.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFID
+{
+    class HttpClientFacde
+    {
+        public const string APPLICATION_FORM = "application/x-www-form-urlencoded; charset=UTF-8";
+        public const string APPLICATION_JSON = "application/json; charset=UTF-8";
+        public const string APPLICATION_XML = "application/xml; charset=UTF-8";
+
+        public static string HttpPost(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = APPLICATION_FORM;
+            request.ContentLength = postDataStr.Length;
+            StreamWriter writer = new StreamWriter(request.GetRequestStream(), Encoding.ASCII);
+            writer.Write(postDataStr);
+            writer.Flush();
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                string encoding = response.ContentEncoding;
+                if (encoding == null || encoding.Length < 1)
+                {
+                    encoding = "UTF-8"; //默认编码  
+                }
+                StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+                string retString = reader.ReadToEnd();
+                return retString;
+            }
+            catch (WebException ex)
+            {
+                return null;
+            }
+            
+        }
+    }
+}
